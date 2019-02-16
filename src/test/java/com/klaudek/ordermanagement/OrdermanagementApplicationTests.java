61e08d03eca0f39f1764be213092054067ec45ef
Klaudiusz <klaudekmen2@gmail.com>
2019-02-16 14:25:50 +0100

    Added project to github with proper dependencies

---
+++ src/test/java/com/klaudek/ordermanagement/OrdermanagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.klaudek.ordermanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdermanagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
